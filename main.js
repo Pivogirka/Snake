1dd23c590e8616e5f708e0ed8a977ca98f768370
Bogdan <bohdan.rohozianskyi@nure.ua>
2023-03-15 22:14:59 +0200

    initialization of the project

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+//dev tools
+const isDev = process.env.NODE_DEV !== 'production';
+//
+const createMainWindow = () => {
+  const mainWindow = new BrowserWindow({
+    title: "The snake game",
+    width: isDev ? 1400 :700,
+    height: 607,
+  })
+
+  //dev tools
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+  //
+
+  mainWindow.loadFile(path.join(__dirname, './public/index.html'))
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
